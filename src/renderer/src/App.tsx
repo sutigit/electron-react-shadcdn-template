da96f40f5d57ef445f67ef834eb84c2894ef6eb5
sutigit <panyanilben@gmail.com>
2025-08-25 12:29:23 +0300

    enhance frontend a bit

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,22 +1,19 @@
 import Versions from './components/Versions'
+import { Button } from "@renderer/components/ui/button"
 
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <p className="text-green-200">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
+    <div className='min-h-screen bg-zinc-900 text-white flex flex-column '>
+      <div className='flex-1'>
+        <h2 className="text-green-200">
+          Press <code>F12</code> to open the devTool
+        </h2>
+        <Button className='bg-green-200 text-zinc-900' onClick={ipcHandle}>Send IPC</Button>
       </div>
       <Versions></Versions>
-    </>
+    </ div>
   )
 }
 
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -4,10 +4,10 @@ function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="p-4 bg-orange-200 items-center">
+      <li className="text-zinc-900 electron-version">Electron v{versions.electron}</li>
+      <li className="text-zinc-900 chrome-version">Chromium v{versions.chrome}</li>
+      <li className="text-zinc-900 node-version">Node v{versions.node}</li>
     </ul>
   )
 }
