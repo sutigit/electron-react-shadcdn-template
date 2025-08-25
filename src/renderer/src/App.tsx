870f05bffafc806549233ebb7499451d8e6c6610
sutigit <panyanilben@gmail.com>
2025-08-25 11:54:24 +0300

    first commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,23 @@
+import Versions from './components/Versions'
+
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <p className="tip">
+        Please try pressing <code>F12</code> to open the devTool
+      </p>
+      <div className="actions">
+        <div className="action">
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+      <Versions></Versions>
+    </>
+  )
+}
+
+export default App
